907403944daf23a347b55e3062b9198948bf7335
faikit <huijie.wang@hotmail.com>
2025-05-31 16:43:49 +0800

    .

--- app/(project)/(ReSim)/page.tsx
+++ app/(project)/(ReSim)/page.tsx
@@ -49,12 +49,12 @@ export default function Home() {
 
 
             <div className="w-full h-screen flex flex-col p-20 snap-start">
-                <div className="w-full h-1/5 flex flex-row justify-center items-center">
+                <div className="w-full h-1/6 flex flex-row justify-center items-center">
                     <h1 className="text-t1 font-bold">
                         Action Control
                     </h1>
                 </div>
-                <div className="w-full h-4/5 flex flex-row justify-center items-center">
+                <div className="w-full h-5/6 flex flex-row justify-center items-center">
                     <div className="w-1/4 h-full flex flex-col gap-20 justify-evenly items-center">
                         <h2 className="text-3xl font-bold">
                             nuScenes
@@ -87,13 +87,13 @@ export default function Home() {
 
 
 
-            <div className="w-full h-screen flex flex-col p-20 snap-start">
-                <div className="w-full h-1/5 flex flex-col gap-6 justify-center items-center">
+            <div className="w-full h-screen flex flex-col p-10 snap-start gap-10">
+                <div className="w-full h-1/6 flex flex-col justify-center items-center">
                     <h1 className="text-t1 font-bold">
                         Action Control
                     </h1>
                 </div>
-                <div className="w-full h-4/5 flex flex-row justify-center items-center">
+                <div className="w-full h-5/6 flex flex-row justify-center items-center">
                     <div className="w-1/4 h-full flex flex-col gap-20 justify-evenly items-center">
                         <h2 className="text-3xl font-bold">
                             Waymo
@@ -126,25 +126,31 @@ export default function Home() {
 
 
 
-            <div className="w-full h-screen flex flex-col p-20 snap-start">
-                <div className="w-full h-1/5 flex flex-col gap-6 justify-center items-center">
+            <div className="w-full h-screen flex flex-col p-10 snap-start gap-10">
+                <div className="w-full h-1/6 flex flex-col justify-center items-center">
                     <h1 className="text-t1 font-bold">
                         Action-Free
                     </h1>
                 </div>
-                <div className="w-full h-4/5 flex flex-row justify-center items-center">
-                    <div className="w-1/4 h-full flex flex-col gap-20 justify-evenly items-center">
-                        <h2 className="text-3xl font-bold">
-                            NAVSIM
-                        </h2>
-                        <h2 className="text-3xl font-bold">
-                            nuScenes
-                        </h2>
-                        <h2 className="text-3xl font-bold">
-                            Waymo
-                        </h2>
+                <div className="w-full h-5/6 flex flex-row justify-center items-center">
+                    <div className="w-1/4 h-full flex flex-col gap-20 justify-between items-center">
+                        <div className="flex-1 flex flex-col justify-center">
+                            <h2 className="text-3xl font-bold">
+                                NAVSIM
+                            </h2>
+                        </div>
+                        <div className="flex-1 flex flex-col justify-center">
+                            <h2 className="text-3xl font-bold">
+                                nuScenes
+                            </h2>
+                        </div>
+                        <div className="flex-1 flex flex-col justify-center">
+                            <h2 className="text-3xl font-bold">
+                                Waymo
+                            </h2>
+                        </div>
                     </div>
-                    <div className="w-3/4 h-full flex flex-col gap-20 justify-evenly items-center">
+                    <div className="w-3/4 h-full flex flex-col gap-20 justify-between items-center">
                         <video
                             preload="none"
                             autoPlay
@@ -178,6 +184,95 @@ export default function Home() {
                     </div>
                 </div>
             </div>
+
+
+
+            <div className="w-full h-screen flex flex-col p-10 snap-start gap-10">
+                <div className="w-full h-1/6 flex flex-col justify-center items-center">
+                    <h1 className="text-t1 font-bold">
+                        Long Horizon
+                    </h1>
+                </div>
+                <div className="w-full h-5/6 flex flex-row justify-center items-center">
+                    <div className="w-full h-full flex flex-col gap-20 justify-evenly items-center">
+                        <video
+                            preload="none"
+                            autoPlay
+                            loop
+                            muted
+                            playsInline
+                            className="w-1/3 object-fit rounded-sm bg-gradient-landing"
+                            >
+                            <source src="https://opendrivelab.github.io/ReSim/long_horizon/long_horizon1.mp4"/>
+                        </video>
+                        <video
+                            preload="none"
+                            autoPlay
+                            loop
+                            muted
+                            playsInline
+                            className="w-1/3 object-fit rounded-sm bg-gradient-landing"
+                            >
+                            <source src="https://opendrivelab.github.io/ReSim/long_horizon/long_horizon1.mp4"/>
+                        </video>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className="w-full h-screen flex flex-col p-10 snap-start gap-10">
+                <div className="w-full h-1/6 flex flex-col justify-center items-center">
+                    <h1 className="text-t1 font-bold">
+                        Comparison
+                    </h1>
+                </div>
+                <div className="w-full h-5/6 flex flex-row justify-center items-center">
+                    <div className="w-full h-full flex flex-col gap-20 justify-evenly items-center">
+                        <video
+                            preload="none"
+                            autoPlay
+                            loop
+                            muted
+                            playsInline
+                            className="w-1/8 object-fit rounded-sm bg-gradient-landing"
+                            >
+                            <source src="https://opendrivelab.github.io/ReSim/method_comparisons/comparison1.mp4"/>
+                        </video>
+                        <video
+                            preload="none"
+                            autoPlay
+                            loop
+                            muted
+                            playsInline
+                            className="w-1/8 object-fit rounded-sm bg-gradient-landing"
+                            >
+                            <source src="https://opendrivelab.github.io/ReSim/method_comparisons/comparison2.mp4"/>
+                        </video>
+                        <video
+                            preload="none"
+                            autoPlay
+                            loop
+                            muted
+                            playsInline
+                            className="w-1/8 object-fit rounded-sm bg-gradient-landing"
+                            >
+                            <source src="https://opendrivelab.github.io/ReSim/method_comparisons/comparison3.mp4"/>
+                        </video>
+                        <video
+                            preload="none"
+                            autoPlay
+                            loop
+                            muted
+                            playsInline
+                            className="w-1/8 object-fit rounded-sm bg-gradient-landing"
+                            >
+                            <source src="https://opendrivelab.github.io/ReSim/method_comparisons/comparison4.mp4"/>
+                        </video>
+                    </div>
+                </div>
+            </div>
+
 
 
             <footer className="w-full mb-24 flex flex-col gab-10 snap-start">
