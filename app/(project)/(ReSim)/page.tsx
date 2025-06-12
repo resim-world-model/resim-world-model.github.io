7ab6c85fd3a80453a4f387a81adf4c70ddba63c7
YTEP-ZHI <ytepjz@gmail.com>
2025-06-12 10:40:04 +0800

    .

--- app/(project)/(ReSim)/page.tsx
+++ app/(project)/(ReSim)/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
                                 arXiv
                             </a>
                             <a
-                                href=""
+                                href="https://github.com/OpenDriveLab/ReSim"
                                 target="_blank"
                                 className="flex items-center gap-2 bg-gray-800 text-white rounded-full px-4 py-2 text-sm hover:bg-gray-700 transition"
                             >
